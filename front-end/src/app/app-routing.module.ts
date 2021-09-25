70788a8189f589166589fd8bcf438e77b56b501b
Mpendulo Sibiya <mpendulodev@gmail.com>
2021-09-25 17:09:48 +0200

    login component :rocket:

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
+import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', component: RegisterComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegisterComponent},
+  {path: '', component: LoginComponent}
 ];
 
 @NgModule({
