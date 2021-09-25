bed22d40209b54dd6f9eba75af1bd30be9ff96a1
Edward <eddymalegale@gmail.com>
2021-09-25 06:20:28 +0200

    Added New feartures

---
+++ tadAPI/users/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+
+STATUS = (
+    (1,"ONLINE"),
+    (0,"OFFLILE")
+)
+
+QUALIFICATION =(
+     (1,"Matric"),
+    (2,"Diploma"),
+     (3,"Bachelor"),
+    (4,"Masters")
+)
+
+
+class Organisation(models.Model):
+    compay=models.OneToOneField(User, on_delete=CASCADE)
+    profile_pic=models.ImageField(default='default.jpg', upload_to="profile_pic")
+    email=models.EmailField(null=False, max_length=20)
+    location= models.CharField(null=False, max_length=30)
+
+    def __str__(self):
+        return f"{self.user.username}  Profile"
+    
+class Learner(models.Model):
+    learner=models.OneToOneField(User, on_delete=CASCADE)
+    profile_pic=models.ImageField(default='default.jpg', upload_to="profile_pic")
+    firstname=models.CharField(null=False, max_length=34)
+    lastname=models.CharField(null=False, max_length=34)
+    username=models.CharField(null=False, max_length=34)
+    email=models.EmailField(null=False, max_length=34)
+    address=models.CharField(null=False, max_length=34)
+
+
+class Tutor(models.Model):
+    tutor=models.OneToOneField(User, on_delete=CASCADE)
+    profile_pic=models.ImageField(default='default.jpg', upload_to="profile_pic")
+    firstname=models.CharField(null=False, max_length=34)
+    lastname=models.CharField(null=False, max_length=34)
+    username=models.CharField(null=False, max_length=34)
+    email=models.EmailField(null=False, max_length=34)
+    address=models.CharField(null=False, max_length=34)
+    high_qualification= models.IntegerField(choices=QUALIFICATION , default=1)
+    specialization=models.CharField(null=False, max_length=30)
+
+# Create your models here.
